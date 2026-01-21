1be655ee3674ae67db1eddba0e7628538ff08797
Yann V <yannv@example.com>
2026-01-21 23:45:58 +0100

    fix: i18n config and build errors for Vercel

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,9 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    typedRoutes: false
+  }
 };
 
 export default withNextIntl(nextConfig);
